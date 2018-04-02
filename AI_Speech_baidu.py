23da64d968c9de9c883363114126fe3c3a9d9d7f
yue <flyyue5@163.com>
2018-04-02 14:34:50 +0800

    Initial commit

---
+++ AI_Speech_baidu.py
@@ -0,0 +1,26 @@
+from aip import AipSpeech
+
+""" 你的 APPID AK SK """
+APP_ID = '11033230'
+API_KEY = '5FRt7PwnBH550tOBgLypHN3G'
+SECRET_KEY = 'S4SEw2HvKaHBqOxv5fz0ysCqRgEDrTLm'
+
+client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+
+# 读取文件
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+# 识别本地文件
+a = client.asr(get_file_content('16k.pcm'), 'pcm', 16000, {
+    'dev_pid': '1536',
+})
+
+# 从URL获取文件识别
+# b = client.asr('', 'pcm', 16000, {
+#     'url': 'http://www.yourdomain.com/res/16k_test.pcm',
+#     'callback': 'http://www.yourdomain.com/post-dump.php',
+# })
+
+print(a)
